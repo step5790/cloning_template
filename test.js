94d05759060c9a5240b09f8b9b75741009d1286d
Stephen <stephen.s.espinosa@gmail.com>
2021-02-08 18:43:13 +0100

    testing

---
+++ test.js
@@ -0,0 +1,25 @@
+const student = {
+  firstName: "George",
+  lastName: "Molly",
+  Nationality: "African",
+  Age: "33",
+  img:
+    "https://images.pexels.com/photos/3118214/pexels-photo-3118214.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+};
+
+// grab the template
+const template = document.querySelector("template").content;
+
+// clone
+const copy = template.cloneNode(true);
+
+copy.querySelector("h1 span").textContent =
+  student.firstName + " " + student.lastName;
+copy.querySelector("p span").textContent = student.Nationality;
+copy.querySelector("img").src = student.img;
+
+// find parent
+const parent = document.querySelector("main");
+
+// append
+parent.appendChild(copy);
